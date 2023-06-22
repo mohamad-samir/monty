62ad6f9f89b37d1b9f00162e21485857fd6bc7f1
mohamad-samir <mohamad.samir2011@gmail.com>
2023-06-22 17:42:28 +0300

    update

--- 1-push.c
+++ 1-push.c
@@ -7,51 +7,39 @@
  * Return: no return
 */
 
-void f_push(stack_t **head, unsigned int counter)
+void _push(stack_t **head, unsigned int counter)
 {
-    int n, j = 0, flag = 0;
+	int n, j = 0;
 
-    /*التحقق مما إذا كان هناك وسيطة تم تمريرها إلى الدالة*/
-    if (bus.arg)
-    {
-        /*التحقق مما إذا كانت الوسيطة رقمًا سالبًا*/
-        if (bus.arg[0] == '-')
-            j++;
+	if (!bus.arg)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
 
-        /*التحقق مما إذا كانت الوسيطة رقمًا صحيحًا*/
-        for (; bus.arg[j] != '\0'; j++)
-        {
-            if (bus.arg[j] > 57 || bus.arg[j] < 48)
-                flag = 1;
-        }
+	if (bus.arg[0] == '-')
+		j++;
 
-        /*إذا كانت الوسيطة غير صالحة، فإنه يتم إخراج رسالة خطأ وإنهاء البرنامج*/
-        if (flag == 1)
-        {
-            fprintf(stderr, "L%d: usage: push integer\n", counter);
-            fclose(bus.file);
-            free(bus.content);
-            free_stack(*head);
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        /*إذا لم يتم تمرير وسيطة، يتم إخراج رسالة خطأ وإنهاء البرنامج*/
-        fprintf(stderr, "L%d: usage: push integer\n", counter);
-        fclose(bus.file);
-        free(bus.content);
-        free_stack(*head);
-        exit(EXIT_FAILURE);
-    }
+	while (bus.arg[j] != '\0')
+	{
+		if (bus.arg[j] > '9' || bus.arg[j] < '0')
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE);
+		}
+		j++;
+	}
 
-    /*تحويل الوسيطة إلى رقم صحيح وحفظه في المتغير n*/
-    n = atoi(bus.arg);
+	n = atoi(bus.arg);
 
-    /*إذا كانت lifi تساوي صفر، يتم إضافة العنصر إلى الستاك (addnode)
-    وإلا، يتم إضافة العنصر إلى قائمة الانتظار (addqueue)*/
-    if (bus.lifi == 0)
-        addnode(head, n);
-    else
-        addqueue(head, n);
+	if (bus.lifi == 0)
+		addnode(head, n);
+	else
+		addqueue(head, n);
 }
