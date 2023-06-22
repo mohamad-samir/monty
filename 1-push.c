d1fda3b2af322fdef2ac595e501495f1faeb5949
mohamad-samir <mohamad.samir2011@gmail.com>
2023-06-22 17:39:44 +0300

    update

--- 1-push.c
+++ 1-push.c
@@ -7,39 +7,51 @@
  * Return: no return
 */
 
-void _push(stack_t **head, unsigned int counter)
+void f_push(stack_t **head, unsigned int counter)
 {
-	int n, j = 0;
+    int n, j = 0, flag = 0;
 
-	if (!bus.arg)
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
+    /*التحقق مما إذا كان هناك وسيطة تم تمريرها إلى الدالة*/
+    if (bus.arg)
+    {
+        /*التحقق مما إذا كانت الوسيطة رقمًا سالبًا*/
+        if (bus.arg[0] == '-')
+            j++;
 
-	if (bus.arg[0] == '-')
-		j++;
+        /*التحقق مما إذا كانت الوسيطة رقمًا صحيحًا*/
+        for (; bus.arg[j] != '\0'; j++)
+        {
+            if (bus.arg[j] > 57 || bus.arg[j] < 48)
+                flag = 1;
+        }
 
-	while (bus.arg[j] != '\0')
-	{
-		if (bus.arg[j] > '9' || bus.arg[j] < '0')
-		{
-			fprintf(stderr, "L%d: usage: push integer\n", counter);
-			fclose(bus.file);
-			free(bus.content);
-			free_stack(*head);
-			exit(EXIT_FAILURE);
-		}
-		j++;
-	}
+        /*إذا كانت الوسيطة غير صالحة، فإنه يتم إخراج رسالة خطأ وإنهاء البرنامج*/
+        if (flag == 1)
+        {
+            fprintf(stderr, "L%d: usage: push integer\n", counter);
+            fclose(bus.file);
+            free(bus.content);
+            free_stack(*head);
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        /*إذا لم يتم تمرير وسيطة، يتم إخراج رسالة خطأ وإنهاء البرنامج*/
+        fprintf(stderr, "L%d: usage: push integer\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
 
-	n = atoi(bus.arg);
+    /*تحويل الوسيطة إلى رقم صحيح وحفظه في المتغير n*/
+    n = atoi(bus.arg);
 
-	if (bus.lifi == 0)
-		addnode(head, n);
-	else
-		addqueue(head, n);
+    /*إذا كانت lifi تساوي صفر، يتم إضافة العنصر إلى الستاك (addnode)
+    وإلا، يتم إضافة العنصر إلى قائمة الانتظار (addqueue)*/
+    if (bus.lifi == 0)
+        addnode(head, n);
+    else
+        addqueue(head, n);
 }
