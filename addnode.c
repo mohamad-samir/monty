82510eeeb9cb373beb3e710120390ccdb6379bec
mohamad-samir <mohamad.samir2011@gmail.com>
2023-06-23 12:29:58 +0300

    update

--- addnode.c
+++ addnode.c
@@ -1,24 +1,33 @@
 #include "monty.h"
+
 /**
- * addnode - add node to the head stack
- * @head: head of the stack
- * @n: new_value
- * Return: no return
-*/
+ * addnode - Add a node to the head of the stack
+ * @head: Pointer to the head of the stack
+ * @n: Value to be stored in the new node
+ *
+ * Return: No return value
+ */
 void addnode(stack_t **head, int n)
 {
-
 	stack_t *new_node, *aux;
 
 	aux = *head;
+	/* Create a new node */
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
-	{ printf("Error\n");
-		exit(0); }
+	{
+		printf("Error: Failed to allocate memory for new node\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Update the links between the nodes */
 	if (aux)
 		aux->prev = new_node;
+
 	new_node->n = n;
 	new_node->next = *head;
 	new_node->prev = NULL;
+
+	/* Update the head of the stack */
 	*head = new_node;
 }
