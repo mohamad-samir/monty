22b03e0ab80aae1e9ac722348906f826231c9b3b
mohamad-samir <mohamad.samir2011@gmail.com>
2023-06-23 12:35:24 +0300

    update

--- addnode.c
+++ addnode.c
@@ -9,24 +9,23 @@
  */
 void addnode(stack_t **head, int n)
 {
-	stack_t *new_node, *aux;
-
-	aux = *head;
 	/* Create a new node */
-	new_node = malloc(sizeof(stack_t));
+	stack_t *new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
 	{
 		printf("Error: Failed to allocate memory for new node\n");
 		exit(EXIT_FAILURE);
 	}
 
-	/* Update the links between the nodes */
-	if (aux)
-		aux->prev = new_node;
-
+	/* Set the value of the new node */
 	new_node->n = n;
+
+	/* Connect the new node to the existing list */
 	new_node->next = *head;
 	new_node->prev = NULL;
+
+	if (*head != NULL)
+		(*head)->prev = new_node;
 
 	/* Update the head of the stack */
 	*head = new_node;
