6bc7860d4325a52cda8d71cbde38c9e65b1cd464
mohamad-samir <mohamad.samir2011@gmail.com>
2023-06-23 13:55:47 +0300

    update

--- queue.c
+++ queue.c
@@ -1,48 +1,58 @@
 #include "monty.h"
+
 /**
- * _queue - prints the top
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
+ * _queue - changes the behavior of adding nodes to the stack to queue mode
+ * @head: pointer to the head of the stack
+ * @counter: line number
+ * Return: No return value
+ */
 void _queue(stack_t **head, unsigned int counter)
 {
+	/* Change the mode to queue */
+	bus.lifi = 1;
+
+	/* Unused parameters */
 	(void)head;
 	(void)counter;
-	bus.lifi = 1;
 }
 
 /**
- * addqueue - add node to the tail stack
- * @n: new_value
- * @head: head of the stack
- * Return: no return
-*/
+ * addqueue - adds a new node at the end of the stack (queue mode)
+ * @n: value to be stored in the new node
+ * @head: pointer to the head of the stack
+ * Return: No return value
+ */
 void addqueue(stack_t **head, int n)
 {
-	stack_t *new_node, *aux;
+	/* Create a new node */
+	stack_t *new_node = malloc(sizeof(stack_t));
 
-	aux = *head;
-	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
 	{
-		printf("Error\n");
+		printf("Error: Memory allocation failed\n");
+		return;
 	}
+
+	/* Set the value of the new node */
 	new_node->n = n;
 	new_node->next = NULL;
-	if (aux)
+
+	if (*head == NULL)
 	{
-		while (aux->next)
-			aux = aux->next;
-	}
-	if (!aux)
-	{
+		/* If the stack is empty, make the new node the head */
 		*head = new_node;
 		new_node->prev = NULL;
 	}
 	else
 	{
-		aux->next = new_node;
-		new_node->prev = aux;
+		/* Find the last node in the stack */
+		stack_t *current = *head;
+
+		while (current->next != NULL)
+			current = current->next;
+
+		/* Append the new node at the end of the stack */
+		current->next = new_node;
+		new_node->prev = current;
 	}
 }
